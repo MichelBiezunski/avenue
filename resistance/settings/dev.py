ddbc592eb9ccb4d2ec2fd37c9efe52689670a710
michel <mb@infoloom.com>
2020-07-23 10:53:41 -0400

    Initial settings. continued

--- resistance/settings/dev.py
+++ resistance/settings/dev.py
@@ -1,18 +1,38 @@
 from .base import *
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+WAGTAILADMINBASE_URL = 'localhost:8000'
+BASE_URL = 'localhost:8000'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#s6%f)uv+liwd^fk$sit#r^p%s!zgh-www+!2nql+si^tf)lh9'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'localhost_static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'localhost_media')
+
+
+DATABASES['default']['NAME'] = 'resistance_db'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
-try:
-    from .local import *
-except ImportError:
-    pass
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'watched_file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/var/log/django/infoloom_website.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['watched_file', ],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    }
+}
